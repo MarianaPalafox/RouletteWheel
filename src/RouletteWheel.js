ce02a14ccdb277f1e0c5a693eea473366d3c7087
MarianaPalafox <mariana.palafox.delateja@gmail.com>
2021-02-02 18:06:12 -0600

    change wheel

--- src/RouletteWheel.js
+++ src/RouletteWheel.js
@@ -1,136 +1,129 @@
-import React, { useEffect, useState } from "react";
-import { Button, Grid, Paper } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button,  DialogContent, Grid, Paper, Dialog, DialogTitle, TextField, Typography } from "@material-ui/core";
+import { Formik } from "formik";
+import { SketchPicker } from 'react-color';
+import ParticlesBg from "particles-bg";
+import { Wheel } from 'react-custom-roulette';
 
 const RouletteWheel = () => {
-    const [options,setOptions] = useState([{ option: '0', style: { backgroundColor: 'green', textColor: 'black' } },{ option: '1', style: { backgroundColor: 'yellow' } },{ option: '2' , style: { backgroundColor: 'blue' } }])
-    var startAngle = 0;
-    var arc = Math.PI / (options.length / 2);
-    var spinTimeout = null;
-    var spinTime = 0;
-    var spinTimeTotal = 0;
-    var spinAngleStart = 0;
-    var ctx;
-    
-    useEffect(() => {
-       drawRouletteWheel()
-    })
-    
-    const rotateWheel = () => {
-        spinTime += 30;
-        if(spinTime >= spinTimeTotal) {
-          stopRotateWheel();
-          return;
-        }
-        var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-        startAngle += (spinAngle * Math.PI / 180);
-        drawRouletteWheel();
-        spinTimeout = setTimeout(rotateWheel(), 30);
+    const [options,setOptions] = useState([{ option: '1', style: { backgroundColor: '#baf2d8', textColor: 'black' } },{ option: '2', style: { backgroundColor: '#bad7f2' } },{ option: '3' , style: { backgroundColor: '#f2bac9' } },{ option: '4' , style: { backgroundColor: '#f2e2ba' } }])
+    const defaultOption = {option: "", style: {backgroundColor: "gray", textColor: 'black'}};
+    const [option,setOption] = useState(null);
+    const [spin,setSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
+    let config = {
+        num: [4, 7],
+        rps: 0.1,
+        radius: [5, 40],
+        life: [1.5, 3],
+        v: [2, 3],
+        tha: [-40, 40],
+        alpha: [0.6, 0],
+        scale: [.1, 0.4],
+        position: "all",
+        color: ["#baf2d8", "#bad7f2", "#f2bac9", "#f2e2ba"],
+        cross: "dead",
+        random: 15
+      };
+    const getSpinning = () => {
+        const newPrizeNumber = Math.floor(Math.random() * options.length)
+        setPrizeNumber(newPrizeNumber)
+        setSpin(true);
     }
-
-    const stopRotateWheel = () => {
-        clearTimeout(spinTimeout);
-        var degrees = startAngle * 180 / Math.PI + 90;
-        var arcd = arc * 180 / Math.PI;
-        var index = Math.floor((360 - degrees % 360) / arcd);
-        ctx.save();
-        ctx.font = 'bold 30px Helvetica, Arial';
-        var text = options[index].option
-        ctx.fillText(text, 250 - ctx.measureText(text).width / 2, 250 + 10);
-        ctx.restore();
-    }
-      
-    const easeOut = (t, b, c, d) => {
-        var ts = (t/=d)*t;
-        var tc = ts*t;
-        return b+c*(tc + -3*ts + 3*t);
-    }
-
-    const spin = () => {
-        spinAngleStart = Math.random() * 10 + 10;
-        spinTime = 0;
-        spinTimeTotal = Math.random() * 3 + 4 * 1000;
-        console.log(spinTimeTotal)
-        rotateWheel();
-    }
-
-    const drawRouletteWheel = () => {
-        var canvas = document.getElementById("canvas");
-        if (canvas.getContext) {
-          var outsideRadius = 200;
-          var textRadius = 160;
-          var insideRadius = 125;
-      
-          ctx = canvas.getContext("2d");
-          ctx.clearRect(0,0,500,500);
-      
-          ctx.strokeStyle = "black";
-          ctx.lineWidth = 2;
-      
-          ctx.font = 'bold 12px Helvetica, Arial';
-      
-          for(var i = 0; i < options.length; i++) {
-            var angle = startAngle + i * arc;
-            //ctx.fillStyle = colors[i];
-            ctx.fillStyle = options[i].style.backgroundColor;
-      
-            ctx.beginPath();
-            ctx.arc(250, 250, outsideRadius, angle, angle + arc, false);
-            ctx.arc(250, 250, insideRadius, angle + arc, angle, true);
-            ctx.stroke();
-            ctx.fill();
-      
-            ctx.save();
-            ctx.shadowOffsetX = -1;
-            ctx.shadowOffsetY = -1;
-            ctx.shadowBlur    = 0;
-            ctx.shadowColor   = "rgb(220,220,220)";
-            ctx.fillStyle = "black";
-            ctx.translate(250 + Math.cos(angle + arc / 2) * textRadius, 
-                          250 + Math.sin(angle + arc / 2) * textRadius);
-            ctx.rotate(angle + arc / 2 + Math.PI / 2);
-            var text = options[i].option;
-            ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-            ctx.restore();
-          } 
-      
-          //Arrow
-          ctx.fillStyle = "black";
-          ctx.beginPath();
-          ctx.moveTo(250 - 4, 250 - (outsideRadius + 5));
-          ctx.lineTo(250 + 4, 250 - (outsideRadius + 5));
-          ctx.lineTo(250 + 4, 250 - (outsideRadius - 5));
-          ctx.lineTo(250 + 9, 250 - (outsideRadius - 5));
-          ctx.lineTo(250 + 0, 250 - (outsideRadius - 13));
-          ctx.lineTo(250 - 9, 250 - (outsideRadius - 5));
-          ctx.lineTo(250 - 4, 250 - (outsideRadius - 5));
-          ctx.lineTo(250 - 4, 250 - (outsideRadius + 5));
-          ctx.fill();
-        }
-      }
-
+    console.log("Spin",spin)
+    console.log("Prize",prizeNumber)
     return (
-        <Grid container spacing={2} style={{padding: 15}}>
-            <Grid item xs={12} sm={3}>
-                <Paper style={{padding:15}}>                    
-                    {
-                        options.map((o,i) => (
-                            <div className="option_grid" key={i}>
-                                <div className="color" style={{background: o.style ? o.style.backgroundColor : "gray"}}/>
-                                <div className="label">
-                                    {o.option}
+        <>
+            <ParticlesBg type="custom" config={config} bg={true}/>
+            <Grid container spacing={2} style={{padding: 15}}>
+                <Grid item xs={12} sm={3}>
+                    <Paper style={{padding:15}}>      
+                        <div style={{maxHeight:"calc(80vh - 100px)", overflowY: "scroll"}}>         
+                        {
+                            options.map((o,i) => (
+                                <div className="option_grid" key={i} onClick={() => setOption(o)}>
+                                    <div className="color" style={{background: o.style ? o.style.backgroundColor : "gray"}}/>
+                                    <div className="label">
+                                        {o.option}
+                                    </div>
                                 </div>
-                            </div>
-                        ))
-                    }                    
-                </Paper>
+                            ))
+                        }  
+                        </div>     
+                    <Button variant="contained" color="secondary" onClick={() => setOption(defaultOption)}>+</Button>                  
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={9}>
+                <center>
+                    <Button variant="contained" color="primary" onClick={() => getSpinning()}>Spin!</Button>
+                    <Wheel mustStartSpinning={spin} priceNumber={prizeNumber} data={options} onStopSpinning={() => setSpin(false)} innerBorderWidth={1}/>             
+                </center>
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={9}>
-            <center>
-                <Button variant="contained" color="primary" onClick={() => spin(true)}>Spin!</Button>
-                <canvas id="canvas" width="500" height="500"></canvas>                
-            </center>
-            </Grid>
-        </Grid>
+            <Dialog open={option !== null} onClose={() => setOption(null)} maxWidth="sm" fullWidth>
+                <DialogTitle>Add new color</DialogTitle>
+                <DialogContent>
+                    <Formik
+                        initialValues={{...option}}
+                        validate={values => {
+                            const errors = {}
+                            if(values.option === ""){
+                                errors.option = "Required";
+                            }
+                            return errors;
+                        }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            const newOptions = options;
+                            console.log("Option",option)
+                            if(option === defaultOption){
+                                newOptions.push(values);
+                                setOptions(newOptions);
+                            } else {
+                                newOptions[options.indexOf(option)] = values;
+                                setOptions(newOptions);
+                            }
+                            
+                            setTimeout(() => {
+                                setSubmitting(false);
+                                setOption(null)
+                            },500)
+                        }}>
+                        {
+                            ({values,errors,touched,handleChange,handleBlur,handleSubmit,isSubmitting,setFieldValue}) => (
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                        label="New Option"
+                                        name="option"
+                                        fullWidth
+                                        variant="outlined"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.option}
+                                        error={errors.option !== undefined && errors.option !== ""}
+                                        helperText={errors.option}/>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography variant="h6">Background Color</Typography>
+                                        <SketchPicker color={values.style.backgroundColor} onChangeComplete={color => setFieldValue("style['backgroundColor']",color.hex)}/>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography variant="h6">Text Color</Typography>
+                                        <SketchPicker color={values.style.textColor} onChangeComplete={color => setFieldValue("style['textColor']",color.hex)}/>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Grid container direction="row" justify="flex-end" alignItems="center">
+                                            <Button variant="contained" color="secondary" onClick={() => setOption(null)}>Cancel</Button>
+                                            <Button variant="contained" color="primary" onClick={handleSubmit} disabled={isSubmitting} style={{marginLeft:10}}>Add</Button>
+                                        </Grid>
+                                    </Grid>                                
+                                </Grid>
+                            )
+                        }
+                    </Formik>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 }
 
